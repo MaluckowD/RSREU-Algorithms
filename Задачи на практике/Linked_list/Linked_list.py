8927ac815f0b7661eda8cf6236b9b0e30b5cc677
Mitya <dim.maluckow2017@yandex.ru>
2024-10-16 16:23:04 +0300

    create all methods for linked list

--- Задачи на практике/Linked_list/Linked_list.py
+++ Задачи на практике/Linked_list/Linked_list.py
@@ -1,43 +1,94 @@
 class Node:
-    """Класс узла односвязного списка."""
     def __init__(self, element, next_node=None):
         self.element = element
         self.next_node = next_node
 
+    def __str__(self):
+        return f'[{self.element} -> {self.next_node}]' 
+
 
 class LinkedList:
-    """Класс односвязного списка."""
     def __init__(self):
         self.head = None
 
-    def get_len(self):
-        """Возвращает длину списка."""
-        if not self.head:
-            return 0
-
-        count = 1
-        current = self.head
-        while current.next_node:
-            count += 1
-            current = current.next_node
-
-        return count
-
     def __str__(self):
-        """Возвращает строковое представление списка."""
-        if not self.head:
+        if self.head is None:
             return "[]"
-
-        result = "["
         current = self.head
-        while current.next_node:
+        result = "["
+        while current:
             result += str(current.element) + ", "
             current = current.next_node
-        result += str(current.element) + "]"
+        result = result[:-2]  
+        result += "]"
         return result
+    
+    def is_empty(self):
+        return self.head is None
+
+    def push_front(self, element):
+        new_node = Node(element, self.head)
+        self.head = new_node
+
+    def push_back(self, element):
+        new_node = Node(element)
+        if self.is_empty():
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next_node:
+                current = current.next_node
+            current.next_node = new_node
+
+    def size(self):
+      count = 0
+      current = self.head
+      while current:
+        count += 1
+        current = current.next_node
+      return count
+
+    def find(self, x):
+        current = self.head
+        while current:
+            if current.element == x:
+                return True
+            current = current.next_node
+        return False
+
+    def pop_front(self):
+        if not self.is_empty():
+            self.head = self.head.next_node
+
+    def pop_back(self):
+        if not self.is_empty():
+            current = self.head
+            previous = None
+            while current.next_node:
+                previous = current
+                current = current.next_node
+            if previous:
+                previous.next_node = None
+            else:
+                self.head = None  
+
+    def remove(self, key):
+        if self.find(key):
+            current = self.head
+            previous = None
+            while current:
+                if current.element == key:
+                    if previous:
+                        previous.next_node = current.next_node
+                    else:
+                        self.head = current.next_node
+                    return True
+                previous = current
+                current = current.next_node
+        return False
+      
 
     def insert(self, element, index):
-        """Вставляет элемент в список по заданному индексу."""
         if index == 0:
             self.head = Node(element, self.head)
             return
@@ -54,8 +105,7 @@ class LinkedList:
         previous.next_node = new_node
 
     def get(self, index):
-        """Возвращает элемент по заданному индексу."""
-        if index < 0 or index >= self.get_len():
+        if index < 0 or index >= self.size():
             raise IndexError("Индекс вне диапазона.")
 
         count = 0
@@ -66,7 +116,7 @@ class LinkedList:
         return current.element
 
     def pop(self, index):
-        if index < 0 or index >= self.get_len():
+        if index < 0 or index >= self.size():
             raise IndexError("Индекс вне диапазона.")
 
         if index == 0:
@@ -85,10 +135,157 @@ class LinkedList:
         element = current.element
         previous.next_node = current.next_node
         return element
+    
+    def reverse(self):
+        previous = None
+        current = self.head
+        next_node = None
+        while current:
+            next_node = current.next_node  
+            current.next_node = previous  
+            previous = current  
+            current = next_node  
+        self.head = previous  
+    
+    def insert_after(self, key, new_element):
+        current = self.head
+        previous = None
+        while current:
+            if current.element == key:
+                new_node = Node(new_element, current.next_node)
+                current.next_node = new_node
+                return True
+            previous = current
+            current = current.next_node
+        return False  
+
+    def insert_before(self, key, new_element):
+        current = self.head
+        previous = None
+        while current:
+            if current.element == key:
+                new_node = Node(new_element, current)
+                if previous:  
+                    previous.next_node = new_node
+                else:  
+                    self.head = new_node
+                return True
+            previous = current
+            current = current.next_node
+        return False 
       
-lst = LinkedList()
-lst.append(10)
-lst.append(20)
-lst.insert(24,2)
-lst.append(80)
-lst.str()
+    def remove_after(self, key):
+        current = self.head
+        previous = None
+        while current:
+            if current.element == key:
+                if current.next_node:
+                    current.next_node = current.next_node.next_node
+                    return True
+                else:
+                    return False 
+            previous = current
+            current = current.next_node
+        return False  
+
+    def remove_before(self, key):
+        current = self.head
+        previous = None
+        while current:
+            if current.next_node and current.next_node.element == key:
+                if previous:
+                    previous.next_node = current.next_node
+                    return True
+                else:
+                    self.head = current.next_node
+                    return True
+            previous = current
+            current = current.next_node
+        return False 
+    
+    def clear(self):
+        self.head = None
+    
+    def count_element(self, key):
+      count = 0
+      current = self.head
+      while current:
+          if current.element == key:
+              count += 1
+          current = current.next_node
+      return count
+    
+    def find_index(self, key):
+        index = 0
+        current = self.head
+        while current:
+            if current.element == key:
+                return index
+            current = current.next_node
+            index += 1
+        return -1
+    
+    def sort(self):
+        if self.head is None or self.head.next_node is None:
+            return 
+
+        current = self.head
+        while current:
+            next_node = current.next_node
+            while next_node:
+                if current.element > next_node.element:
+                    current.element, next_node.element = next_node.element, current.element
+                next_node = next_node.next_node
+            current = current.next_node
+
+    def copy(self):
+        new_list = LinkedList()
+        current = self.head
+        while current:
+            new_list.push_back(current.element)
+            current = current.next_node
+        return new_list
+        
+mylist = LinkedList()
+mylist.push_front(5)
+mylist.push_front(1)
+mylist.push_back(3)
+mylist.push_back(9)
+print(mylist)
+mylist.pop_back()
+print(mylist)
+mylist.pop_front()
+print(mylist)
+mylist.insert(8,1)
+print(mylist)
+
+print(mylist.get(2))
+print(mylist)
+
+
+mylist.pop(0)
+
+print(mylist)
+
+mylist.reverse()
+print(mylist)
+
+mylist.insert_after(10, 8)
+print(mylist)
+
+mylist.insert_before(8, 10)
+print(mylist)
+
+
+mylist.remove_after(100)
+print(mylist)
+
+print(mylist.count_element(11))
+print(mylist.find_index(10))
+mylist.sort()
+print(mylist)
+a = mylist.copy()
+print(a)
+
+mylist.clear()
+print(mylist)
